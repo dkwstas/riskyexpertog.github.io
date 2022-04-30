b6ac6ba729f52b1f9a63f8eb81065f9155095cb6
Kostas <gedra100sh@gmail.com>
2022-04-30 19:11:03 +0300

    New site

--- axios.js
+++ axios.js
@@ -6,9 +6,6 @@ const getData = () => {
   $.ajax({                                      
     url: 'https://www.iep.edu.gr/services/mitroo/trapeza/public/searchsubjects.php?schooltype=1&class=2&lesson=18&subject=2',       
     type: "GET"
-    success: function(data){
-      console.log(data)
-    }
 });
 };
 
